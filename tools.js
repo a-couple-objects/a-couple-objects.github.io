ad1fc4309a3f23c42d464da28de171058407b6d7
a-couple-objects <trulymewhen@gmail.com>
2021-02-20 14:01:31 -0600

    Delete tools.js

--- tools.js
+++
@@ -1,7 +0,0 @@
-function mRequest(ctype, rtype, location, body) {
-              var eeyy = new XMLHttpRequest();
-	              eeyy.open(rtype, location);
-           eeyy.setRequestHeader('Content-type', ctype);
-	eeyy.send(JSON.stringify(body));
-	console.log(body);
-  };
