d192ef2dc57191455df91f5f76dbf54bdb7e7219
a-couple-objects <trulymewhen@gmail.com>
2021-02-20 13:47:33 -0600

    Update tools.js

--- tools.js
+++ tools.js
@@ -2,6 +2,6 @@ function mRequest(ctype, rtype, location, body) {
               var eeyy = new XMLHttpRequest();
 	              eeyy.open(rtype, location);
            eeyy.setRequestHeader('Content-type', ctype);
-	eeyy.send(JSON.stringify(body););
+	eeyy.send(JSON.stringify(body));
 	console.log(body);
   };
