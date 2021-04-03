88256e318890f634bdd9496d1f55baabe1cb7a97
a-couple-objects <trulymewhen@gmail.com>
2021-04-03 02:37:23 -0500

    Update censor.js

--- dis-den/resources/censor.js
+++ dis-den/resources/censor.js
@@ -6,10 +6,10 @@ function censor(input) {
     d = c.replace(/ass/g, "a--");
     e = d.replace(/cunt/g, "c--t");
     f = e.replace(/fire/g, "burny stuff");
-    g = f.replace(/drugs/g, "dr-gs");'
+    g = f.replace(/drugs/g, "dr-gs");
     h = g.replace(/drunk/g, "dr-nk");
     i = h.replace(/blood/g, "cherry kool-aid");
-    j = i.replace(/hate/g, "h-te");'
+    j = i.replace(/hate/g, "h-te");
     k = j.replace(/school/g, "brain house");
 };
     function copy() {
