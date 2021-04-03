1291acf3a9c313c1766252c48b73ab76056f9a4b
a-couple-objects <trulymewhen@gmail.com>
2021-04-03 02:11:12 -0500

    Update censor.js

--- dis-den/resources/censor.js
+++ dis-den/resources/censor.js
@@ -2,4 +2,11 @@ function censor(input) {
   escinput = input.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
     a = escinput.replace(/fuck/g, "f-ck");
     b = a.replace(/shit/g, "sh-t");
-      document.write(\<h1\>b\<h1/\>);
+    c = b.replace(/bitch/g, "b-tch");
+    d = c.replace(/ass/g, "a--");
+    e = d.replace(/cunt/g, "c--t");
+    f = e.replace(/fire/g, "burny stuff");
+    g = f.replace(/drugs/g, "dr-gs");'
+    h = g.replace(/drunk/g, "dr-nk");
+    i = h.replace(/blood/g, "cherry kool-aid");
+    j = i.replace(/_/g, "-");
