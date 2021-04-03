2f89c565412ba62e1a16d11a2104a50ffee0b99e
a-couple-objects <trulymewhen@gmail.com>
2021-04-03 02:52:06 -0500

    Update censor.js

--- dis-den/resources/censor.js
+++ dis-den/resources/censor.js
@@ -5,12 +5,13 @@ function censor(input) {
     c = b.replace(/bitch/g, "b-tch");
     d = c.replace(/ass/g, "a--");
     e = d.replace(/cunt/g, "c--t");
-    f = e.replace(/fire/g, "burny stuff");
+    f = e.replace(/fire/g, "f-r-");
     g = f.replace(/drugs/g, "dr-gs");
     h = g.replace(/drunk/g, "dr-nk");
-    i = h.replace(/blood/g, "cherry kool-aid");
+    i = h.replace(/blood/g, "bl--d");
     j = i.replace(/hate/g, "h-te");
-    k = j.replace(/school/g, "brain house");
+    k = j.replace(/school/g, "sch--l");
+    j = l.replace(/parent/g, "p-r-nt");
 };
     function copy() {
       copyText=document.getElementById("censortext");copyText.select();copyText.setSelectionRange(0,99999);document.execCommand("copy");
